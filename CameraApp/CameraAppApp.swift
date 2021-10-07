c17339830690ae17fb85abf361185a3d6b089b79
honma <honmatchy@icloud.com>
2021-10-07 14:38:03 +0900

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by 本間光 on 2021/10/07.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
